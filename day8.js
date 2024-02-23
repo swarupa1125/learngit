092982481c49239fc335458f3077aca9f3e4b4c4
swarupa1125 <159742245+swarupa1125@users.noreply.github.com>
2024-02-23 17:33:50 +0530

    Add files via upload

---
+++ day8.js
@@ -0,0 +1,70 @@
+// JAVA SCRIPT
+// java script is a scripting language | loosely coupled programming language | uses OOPS Concepts and also uses without OOPS Concept | Array can be directly called without using for loop | Multiple data types can be used in an array 
+// Data types : primitive and non primitive
+// primitive : single value - boolean , numbers and string
+// Non-primitive : multiple value - Array and object 
+// Operation : Arithmetic , logic , realtive 
+
+// PRIMITIVE DATA-TYPE
+
+// Boolean
+let a=2
+let b=3
+console.log("Boolean",a>b)
+
+let c=true
+console.log(c)
+
+//number
+let d=100
+console.log("number",100)
+let e=100.94209237450927333340952098
+console.log(e)
+
+// String
+let word = "hello world"
+console.log("String",word)
+let word1='hello there'
+console.log(word1)
+
+//NON PRIMITIVE
+
+//Array
+
+let array=[1,2,3,'hello',true]
+console.log(array)
+console.log(array[2])
+array[2]="its me"
+console.log(array)
+console.log(array[2])
+
+// OPERATORS :- Arithmetic , Logic , Comparison , Trinary
+
+// Arithmetic 
+console.log("Arithmetic")
+console.log(2+3)
+console.log(2-3)
+console.log(2/2)
+console.log(2%2)
+
+// Logic 
+console.log("Logic")
+console.log(3>2 && 2>3) // False
+console.log(3>2 || 2>3) // True
+
+//Comparison
+console.log(3>=2)
+
+//Trinary Operator
+let trenary = (10>3)? "yes it is": "No bro"
+console.log(trenary)
+
+
+
+
+
+
+
+
+
+
